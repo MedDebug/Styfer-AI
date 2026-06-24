4832024019c2b2b99c48d967cd1c88ade03b8c48
Medhansh Singh <medhanshsingh2006@gmail.com>
2026-06-24 21:32:45 +0530

    Built official memory menu for memory manager

---
+++ memory_menu.py
@@ -0,0 +1,11 @@
+print("-------------------------------------------")
+print("------------Styfer Memory Manager------------")
+print("-------------------------------------------")
+print()
+print("1 - View Memory")
+print("2 - Add New Memory Entry")
+print("3 - Delete A Memory Entry")
+print("4 - Change User Details")
+print("5 - Exit")
+
+
